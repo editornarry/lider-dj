9a87345985a074ba676357dd5e68146e4348b376
editornarry <editor.narrymonpsena@gmail.com>
2026-04-27 01:54:43 -0400

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -4,9 +4,13 @@ let gainA = ctx.createGain();
 let gainB = ctx.createGain();
 let master = ctx.createGain();
 
+let analyser = ctx.createAnalyser();
+analyser.fftSize = 256;
+
 gainA.connect(master);
 gainB.connect(master);
-master.connect(ctx.destination);
+master.connect(analyser);
+analyser.connect(ctx.destination);
 
 function connectDecks(a, b){
   const srcA = ctx.createMediaElementSource(a);
@@ -17,11 +21,14 @@ function connectDecks(a, b){
 }
 
 function crossfade(x){
-  // equal power curve
   gainA.gain.value = Math.cos(x * 0.5 * Math.PI);
   gainB.gain.value = Math.cos((1 - x) * 0.5 * Math.PI);
 }
 
 function setMaster(v){
   master.gain.value = v;
+}
+
+function getAnalyser(){
+  return analyser;
 }
