0636de184eb1e819b4ba67a7d4853343e45e2810
editornarry <editor.narrymonpsena@gmail.com>
2026-04-27 01:42:03 -0400

    Update visuals.js

--- visuals.js
+++ visuals.js
@@ -1,15 +1,36 @@
 const canvas = document.getElementById("viz");
 const ctx2 = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const analyser = ctx.createAnalyser();
+analyser.fftSize = 256;
+
+master.connect(analyser);
+
+const data = new Uint8Array(analyser.frequencyBinCount);
+
 function draw(){
+  requestAnimationFrame(draw);
+
+  analyser.getByteFrequencyData(data);
+
   ctx2.clearRect(0,0,canvas.width,canvas.height);
 
-  for(let i=0;i<50;i++){
-    let h = Math.random()*200;
-    ctx2.fillStyle = "cyan";
-    ctx2.fillRect(i*10, 200-h, 5, h);
-  }
+  let barWidth = (canvas.width / data.length) * 2;
 
-  requestAnimationFrame(draw);
+  data.forEach((v,i)=>{
+    let h = v * 2;
+
+    let gradient = ctx2.createLinearGradient(0,0,0,canvas.height);
+    gradient.addColorStop(0, "#00eaff");
+    gradient.addColorStop(1, "#0011ff");
+
+    ctx2.fillStyle = gradient;
+
+    ctx2.fillRect(i*barWidth, canvas.height-h, barWidth-2, h);
+  });
 }
+
 draw();
