5de8398d8fb5dcaec72573c2045fb39afc3ece03
editornarry <editor.narrymonpsena@gmail.com>
2026-04-27 01:55:04 -0400

    Update visuals.js

--- js/visuals.js
+++ js/visuals.js
@@ -4,11 +4,7 @@ const ctx2 = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const analyser = ctx.createAnalyser();
-analyser.fftSize = 256;
-
-master.connect(analyser);
-
+const analyser = getAnalyser();
 const data = new Uint8Array(analyser.frequencyBinCount);
 
 function draw(){
@@ -18,16 +14,12 @@ function draw(){
 
   ctx2.clearRect(0,0,canvas.width,canvas.height);
 
-  let barWidth = (canvas.width / data.length) * 2;
+  let barWidth = canvas.width / data.length;
 
   data.forEach((v,i)=>{
     let h = v * 2;
 
-    let gradient = ctx2.createLinearGradient(0,0,0,canvas.height);
-    gradient.addColorStop(0, "#00eaff");
-    gradient.addColorStop(1, "#0011ff");
-
-    ctx2.fillStyle = gradient;
+    ctx2.fillStyle = `hsl(${i*2}, 100%, 50%)`;
 
     ctx2.fillRect(i*barWidth, canvas.height-h, barWidth-2, h);
   });
